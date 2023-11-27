e5ed36da09bd65c2e4b48648d74469e63524e637
Trevor Flint <t.flint0824@gmail.com>
2023-11-27 15:42:47 -0500

    testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import home from '../pages/home.vue'
+import vintage from '../pages/vintage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,7 @@ const router = createRouter({
     {
       path: '/vintage',
       name: 'vintage',
-      component: () => import('../pages/vintage.vue')
+      component: vintage
     },
     {
       path: '/betsy',
